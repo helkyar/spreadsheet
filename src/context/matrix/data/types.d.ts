61810914ebba821f07cfb7187b5e0300556456d2
helkyar <javierpalaciosbotejara@gmail.com>
2024-11-11 16:21:33 +0100

    set up tests files

--- src/context/matrix/data/types.d.ts
+++ src/context/matrix/data/types.d.ts
@@ -4,10 +4,49 @@ export type Cell = {
   inputValue: string
   computedValue: string
   update: (value: string) => void
-  id: string
-  references: string[]
 }
 export type MatrixContext = {
   matrix: Cell[][]
   save: () => void
 }
+
+export type ConstructorParams =
+  | { rows: number; cols: number; matrix: undefined }
+  | { matrix: Cell[][]; rows: undefined; cols: undefined }
+
+export type MatrixParams = {
+  rows: number
+  cols: number
+}
+
+export type Coordinates = {
+  x: number
+  y: number
+}
+
+export type PartialCell = Coordinates & {
+  inputValue: string
+}
+
+export type UpdateCellValues = Coordinates & {
+  inputValue?: string
+  expression: string
+}
+
+export type CreateRefValues = Coordinates & {
+  refArray: RegExpMatchArray
+  expression: string
+}
+
+export type RefIndexArray = Coordinates & {
+  ref: string
+}
+
+export type ListOfReferences = Coordinates & {
+  refIndexArray: RefIndexArray[]
+  expression: string
+}
+
+export type ReturnedProcessInput =
+  | { expression: string; refArray: null }
+  | { expression: string; refArray: RegExpMatchArray }
