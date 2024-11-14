9aea98548d3496e5524b2c18d6cba87b579a28b3
helkyar <javierpalaciosbotejara@gmail.com>
2024-11-14 15:52:04 +0100

    add tab styles

--- src/components/Spreadsheet/index.tsx
+++ src/components/Spreadsheet/index.tsx
@@ -16,38 +16,44 @@ function SpreadSheet() {
   } = spreadsheetEntity
 
   return (
-    <table key={id} onClick={removeSelection}>
-      <thead>
-        <tr>
-          {getColumnsHeaderLabels(matrix[0]?.length).map((columLabel, y) => (
-            <th key={columLabel} onClick={selectColumn(y)}>
-              <RemoveCellsIcon
-                isHidden={y < 1}
-                onClick={removeColumn(y - 1)}
-                isVertical
-              />
-              {columLabel}
-              <AddCellsIcon onClick={addColumn(y)} isVertical />
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {matrix?.map((row, x) => (
-          <tr key={x}>
-            <th onClick={selectRow(x)}>
-              <AddCellsIcon onClick={addRow(x)} isHorizontal isHidden={x > 0} />
-              <RemoveCellsIcon isHorizontal onClick={removeRow(x)} />
-              <AddCellsIcon onClick={addRow(x + 1)} isHorizontal />
-              {x + 1}
-            </th>
-            {row.map((cell, y) => (
-              <Cell key={cell.id} cellValues={cell} x={x} y={y} />
+    <section className='table-wrapper'>
+      <table key={id} onClick={removeSelection}>
+        <thead>
+          <tr>
+            {getColumnsHeaderLabels(matrix[0]?.length).map((columLabel, y) => (
+              <th key={columLabel} onClick={selectColumn(y)}>
+                <RemoveCellsIcon
+                  isHidden={y < 1}
+                  onClick={removeColumn(y - 1)}
+                  isVertical
+                />
+                {columLabel}
+                <AddCellsIcon onClick={addColumn(y)} isVertical />
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {matrix?.map((row, x) => (
+            <tr key={x}>
+              <th onClick={selectRow(x)}>
+                <AddCellsIcon
+                  onClick={addRow(x)}
+                  isHorizontal
+                  isHidden={x > 0}
+                />
+                <RemoveCellsIcon isHorizontal onClick={removeRow(x)} />
+                <AddCellsIcon onClick={addRow(x + 1)} isHorizontal />
+                {x + 1}
+              </th>
+              {row.map((cell, y) => (
+                <Cell key={cell.id} cellValues={cell} x={x} y={y} />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   )
 }
 
--- src/context/matrix/MatrixProvider.tsx
+++ src/context/matrix/MatrixProvider.tsx
@@ -1,16 +1,7 @@
 import { Matrix } from '@/context/matrix/data/types'
-// import ComputedMatrix from '@/context/matrix/ComputedMatrix'
 import { MatrixContext } from '@/context/matrix/MatrixContext'
 import { useLocalStorage } from '@/logic/useLocalStorage'
-import {
-  MouseEvent,
-  ReactNode,
-  useCallback,
-  // useEffect,
-  // useRef,
-  useState,
-} from 'react'
-// import { SingletonMatrix } from '@/context/matrix/SingletonMatrix'
+import { MouseEvent, ReactNode, useCallback, useState } from 'react'
 import ComputedMatrix from '@/context/matrix/ComputedMatrix'
 import { v4 as uuidv4 } from 'uuid'
 import { toast } from '@/components/ui/toast'
@@ -26,8 +17,8 @@ const LOCAL_KEY = '_spreadsheet_'
 export const MatrixProvider = ({ children, rows, cols }: Params) => {
   const [matrices, setMatrices] = useLocalStorage<Matrix[]>({ key: LOCAL_KEY })
 
-  const [, setCount] = useState(0)
-  const updater = () => setCount((c) => c + 1)
+  const [, setBoolean] = useState(false)
+  const updater = () => setBoolean((b) => !b)
 
   const [matrixArray, setMatrixArray] = useState<
     { id: string; spreadsheet: ComputedMatrix }[]
@@ -64,6 +55,8 @@ export const MatrixProvider = ({ children, rows, cols }: Params) => {
   }
 
   const removeMatrix = (index: number) => {
+    console.log('🚀 ~ removeMatrix ~ index:', index)
+
     if (matrixArray.length === 1) {
       toast.error('Create another sheet to delete this one')
       return
@@ -74,8 +67,8 @@ export const MatrixProvider = ({ children, rows, cols }: Params) => {
       copy.splice(index, 1)
       return copy
     })
-    setMatrixIdx((prev) => prev - 1)
 
+    if (index < matrixIdx) setMatrixIdx((prev) => prev - 1)
     if (index === matrixIdx) setMatrixIdx(0)
   }
 
@@ -99,6 +92,7 @@ export const MatrixProvider = ({ children, rows, cols }: Params) => {
   const value = {
     spreadsheetEntity: matrixArray[matrixIdx], // Spreadsheet matrix render
     matrixArray,
+    matrixIdx,
     viewMatrix,
     createNewMatrix,
     removeMatrix,
